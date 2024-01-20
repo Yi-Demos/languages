e1e5ce5cef733913a4f3e7163bdb8bc2115b3b13
Tranquility <admin@tranquility.tech>
2024-01-21 03:44:49 +0800

    update

--- basic/call_methods.cpp
+++ basic/call_methods.cpp
@@ -1,14 +1,18 @@
 // https://wayne265265.pixnet.net/blog/post/112556555-%E3%80%90%E6%95%99%E5%AD%B8%E3%80%91call-by-value%2C-call-by-address%2C-call-by-referenc
 
 #include <stdio.h>
+
 #include <iostream>
+
 using namespace std;
 
 // Call by value
 //
-// 當你利用call by value的方法去傳值時因為a與b的記憶體是分開的，所以不會互相干擾，
-// 但也需要兩個記憶體去儲存他們，這時候請仔細思考是否有使用call by value的必要，如果之後在較大型的程式運行時，
-// 若使用太多call by value而使用結束後沒有刪除掉未使用的記憶體空間，可能會導致記憶體不足而程式崩潰，切記切記!
+// 當你利用call by
+// value的方法去傳值時因為a與b的記憶體是分開的，所以不會互相干擾，
+// 但也需要兩個記憶體去儲存他們，這時候請仔細思考是否有使用call by
+// value的必要，如果之後在較大型的程式運行時， 若使用太多call by
+// value而使用結束後沒有刪除掉未使用的記憶體空間，可能會導致記憶體不足而程式崩潰，切記切記!
 
 // int main()
 // {
@@ -44,24 +48,21 @@ using namespace std;
 
 // Call by reference (C++ only)
 
-void swap(int &address_a, int &address_b)
-{
+void swap(int &address_a, int &address_b) {
   int temp = address_a;
   address_a = address_b;
   address_b = temp;
 }
 
-int main()
-{
-
+int main() {
   int a = 1;
   int b = 0;
 
-  cout << a << " " << b << endl; // 1 0
+  cout << a << " " << b << endl;  // 1 0
 
-  swap(a, b); // 傳入a, b的address進行交換
+  swap(a, b);  // 傳入a, b的address進行交換
 
-  cout << a << " " << b << endl; // 0 1
+  cout << a << " " << b << endl;  // 0 1
 
   return 0;
 }
--- problems/1_two_sum.cpp
+++ problems/1_two_sum.cpp
@@ -4,31 +4,31 @@
 using namespace std;
 
 class Solution {
-   public:
-    vector<int> twoSum(vector<int> &nums, int target) {
-        int size = nums.size();
+ public:
+  vector<int> twoSum(vector<int> &nums, int target) {
+    int size = nums.size();
 
-        for (int i = 0; i < size; i++) {
-            for (int y = i + 1; y < size; y++) {
-                if (nums[i] + nums[y] == target) {
-                    cout << i << y << endl;
+    for (int i = 0; i < size; i++) {
+      for (int y = i + 1; y < size; y++) {
+        if (nums[i] + nums[y] == target) {
+          cout << i << y << endl;
 
-                    return {i, y};
-                }
-            }
+          return {i, y};
         }
-
-        // for no solution
-        return {};
+      }
     }
+
+    // for no solution
+    return {};
+  }
 };
 
 int main() {
-    vector<int> v = {2, 7, 11, 15};
-    int target = 9;
+  vector<int> v = {2, 7, 11, 15};
+  int target = 9;
 
-    Solution solution;
-    solution.twoSum(v, target);
+  Solution solution;
+  solution.twoSum(v, target);
 
-    return 0;
+  return 0;
 }
