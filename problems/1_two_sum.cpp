2bf778bb076afb22445443c93c66cc60e79417f2
Tranquility <admin@tranquility.tech>
2024-01-21 02:26:07 +0800

    update

--- problems/1_two_sum.cpp
+++ problems/1_two_sum.cpp
@@ -3,33 +3,32 @@
 
 using namespace std;
 
-class Solution
-{
-public:
-  vector<int> twoSum(vector<int> &nums, int target)
-  {
-    int size = nums.size();
+class Solution {
+   public:
+    vector<int> twoSum(vector<int> &nums, int target) {
+        int size = nums.size();
 
-    for (int i = 0; i < size; i++)
-    {
-      for (int y = i + 1; y < size; y++)
-      {
-        if (nums[i] + nums[y] == target)
-          return {i, y};
-      }
+        for (int i = 0; i < size; i++) {
+            for (int y = i + 1; y < size; y++) {
+                if (nums[i] + nums[y] == target) {
+                    cout << i << y << endl;
+
+                    return {i, y};
+                }
+            }
+        }
+
+        // for no solution
+        return {};
     }
-
-    return {};
-  }
 };
 
-int main()
-{
-  vector<int> v = {2, 7, 11, 15};
-  int target = 9;
+int main() {
+    vector<int> v = {2, 7, 11, 15};
+    int target = 9;
 
-  Solution solution;
-  solution.twoSum(v, target);
+    Solution solution;
+    solution.twoSum(v, target);
 
-  return 0;
+    return 0;
 }
