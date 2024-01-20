305f0f2d760a19ef8f6867b0ab800ce69fbfb5f5
Tranquility <admin@tranquility.tech>
2024-01-21 02:18:34 +0800

    git init

---
+++ basic/c/puts.c
@@ -0,0 +1,10 @@
+# include <stdio.h>
+
+int main(void)
+{
+    char name[] = "输出输出输出输出!";
+    printf("%s\n", name);  //用printf输出
+    puts(name);  //用puts()输出
+    puts("输出输出输出输出!");  //直接输出字符串
+    return 0;
+}
