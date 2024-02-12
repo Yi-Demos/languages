0ea8468116949e19928860e7d0388acb22bcdcd9
Ta-Yi Cheng <admin@tranquility.tech>
2024-02-12 23:20:27 +0800

    update

--- c/problems/169-majority-element.cpp
+++ c/problems/169-majority-element.cpp
@@ -5,11 +5,26 @@ using namespace std;
 
 class Solution {
  public:
-  int majorityElement(vector<int>& nums) {}
+  int majorityElement(vector<int>& nums) {
+    unordered_map<int, int> hash;
+    int maxKey = 0;
+    int maxValue = 0;
+
+    for (int num : nums) {
+      hash[num] = 1 + hash[num];
+
+      if (hash[num] > maxValue) {
+        maxKey = num;
+        maxValue = hash[num];
+      }
+    }
+
+    return maxKey;
+  }
 };
 
 int main() {
-  vector<int> v = {-1, 0, 1, 2, -1, -4};
+  vector<int> v = {3, 2, 3};
 
   Solution solution;
   solution.majorityElement(v);
