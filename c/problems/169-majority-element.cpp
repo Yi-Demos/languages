07845765dc1e80ac5c5a5b878c5046da27fe963b
Ta-Yi Cheng <admin@tranquility.tech>
2024-02-12 22:48:52 +0800

    update

---
+++ c/problems/169-majority-element.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+ public:
+  int majorityElement(vector<int>& nums) {}
+};
+
+int main() {
+  vector<int> v = {-1, 0, 1, 2, -1, -4};
+
+  Solution solution;
+  solution.majorityElement(v);
+
+  return 0;
+}
