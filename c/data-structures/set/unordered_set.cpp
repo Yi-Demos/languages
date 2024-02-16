2a0ac4b84b4a0562ea30cf94d0c753db4bf2cbc5
Ta-Yi Cheng <admin@tranquility.tech>
2024-02-16 23:57:31 +0800

    update

--- c/data-structures/set/unordered_set.cpp
+++ c/data-structures/set/unordered_set.cpp
@@ -5,10 +5,27 @@
 
 using namespace std;
 
+// https://www.programiz.com/cpp-programming/unordered-set
+// unordered_set Methods Description
+// insert()	inserts an element to the unordered set
+// count()	returns 1 if the specified value exists and 0 if it doesn't
+// find()	returns the iterator to the element with the specified value
+// size()	returns the number of unique elements
+// empty()	returns true if the unordered set is empty
+// erase()	removes elements with the specified value
+// clear()	removes all elements
+
+void display(const unordered_set<int> &);
+
 int main() {
   // vector<int> nums;
   // unordered_set<int> ust(nums.begin(), nums.end());
   unordered_set<int> ust = {1, 2, 3, 4, 5};
+
+  // insert ont
+  ust.insert(6);
+  // insert many
+  ust.insert({7, 8, 9});
 
   // check if num has alreay existed in set
   if (ust.count(1)) {
@@ -17,12 +34,21 @@ int main() {
     cout << "not found";
   }
 
-  cout << "\n\n";
+  cout << ust.find(1);
 
-  cout << "numbers = ";
-  for (const auto &num : ust) {
-    cout << num << ", ";
-  }
+  display(ust);
+
+  ust.erase(5);
+
+  display(ust);
 
   return 0;
+}
+
+void display(const unordered_set<int> &uset) {
+  cout << "numbers = ";
+
+  for (const auto &value : uset) {
+    cout << value << ", ";
+  }
 }
