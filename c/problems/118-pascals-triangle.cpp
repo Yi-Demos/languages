5b16bec12cee9e3fd791cf771bde946b22c96406
Ta-Yi Cheng <admin@tranquility.tech>
2024-02-17 03:37:12 +0800

    update

---
+++ c/problems/118-pascals-triangle.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+ public:
+  vector<vector<int>> generate(int numRows) {}
+};
+
+int main() {
+  Solution solution;
+
+  cout << solution.generate();
+
+  cout << "\n\n";
+  return 0;
+}
