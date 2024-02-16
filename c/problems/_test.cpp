6e772e962584894cbeabbc9f62df08aa21e95321
Ta-Yi Cheng <admin@tranquility.tech>
2024-02-16 11:22:31 +0800

    update

--- c/problems/_test.cpp
+++ c/problems/_test.cpp
@@ -6,48 +6,14 @@ using namespace std;
 // https://leetcode.com/problems/3sum/solutions/1462423/c-both-two-pointers-and-hashmap-approach-explained/
 class Solution {
  public:
-  vector<vector<int>> threeSum(vector<int>& nums) {
-    if (nums.size() < 3) return {};
-
-    sort(nums.begin(), nums.end());
-
-    if (nums[0] > 0) return {};
-
-    vector<vector<int>> v;
-
-    for (int i = 0; i < nums.size(); ++i) {
-      if (nums[i] > 0) break;
-      if (i > 0 && nums[i] == nums[i - 1]) continue;
-
-      int l = i + 1;
-      int r = nums.size() - 1;
-      int sum = 0;
-
-      while (l < r) {
-        sum = nums[i] + nums[l] + nums[r];
-
-        if (sum > 0) {
-          r--;
-        } else if (sum < 0) {
-          l++;
-        } else {
-          v.push_back({nums[i], nums[l], nums[r]});
-          cout << nums[i] << nums[l] << nums[r] << endl;
-
-          l++;
-        }
-      }
-    }
-
-    return v;
-  }
+  vector<int> result(vector<int> &v) { return v; }
 };
 
 int main() {
-  vector<int> v = {-1, 0, 1, 2, -1, -4};
+  vector<int> v = {1};
 
   Solution solution;
-  solution.threeSum(v);
+  solution.result(v);
 
   return 0;
 }
