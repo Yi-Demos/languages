106431ce61ae5cc0e9065b4e19077383ab632762
Ta-Yi Cheng <admin@tranquility.tech>
2024-02-17 09:01:31 +0800

    update

--- js/problems/118-pascals-triangle.js
+++ js/problems/118-pascals-triangle.js
@@ -2,6 +2,29 @@
  * @param {number} numRows
  * @return {number[][]}
  */
+
+//
+// Combinatorial Formula
+//
+// var generate = function (numRows) {
+//   const dp = [[1]];
+
+//   for (let i = 0; i < numRows - 1; i++) {
+//       let prevRow = dp[i];
+//       let cur = [1];
+
+//       for (let j = 0; j < prevRow.length - 1; j++) {
+//           cur[j + 1] = prevRow[j] + prevRow[j + 1];
+//       }
+
+//       cur.push(1);
+
+//       dp.push(cur);
+//   }
+
+//   return dp;
+// };
+
 var generate = function (numRows) {
   const dp = [[1]];
 
