623cf39ba3147ac706d68e6b0c9a804495b5f200
Ta-Yi Cheng <admin@tranquility.tech>
2024-02-25 03:31:40 +0800

    update

---
+++ js/problems/27-remove-element.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+  let cur = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== val) {
+      nums[cur] = nums[i];
+
+      cur++;
+    }
+  }
+
+  return nums;
+};
+
+console.log(removeElement([3,2,2,3], 3));
