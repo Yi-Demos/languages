d5e10f9fd3f25459ea571363ebf84d7e6857c65e
Ta-Yi Cheng <admin@tranquility.tech>
2024-02-27 22:43:14 +0800

    update

---
+++ js/problems/dist/2634-filter-elements-from-array.dev.js
@@ -0,0 +1,10 @@
+"use strict";
+
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var filter = function filter(arr, fn) {
+  return arr.filter(fn);
+};
---
+++ js/problems/dist/2635-apply-transform-over-each-element-in-array.dev.js
@@ -0,0 +1,10 @@
+"use strict";
+
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var map = function map(arr, fn) {
+  return arr.map(fn);
+};
