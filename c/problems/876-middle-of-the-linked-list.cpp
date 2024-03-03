f319b3f16211407015c968ec88100a0cf82a4924
Ta-Yi Cheng <admin@tranquility.tech>
2024-03-03 21:58:33 +0800

    update

---
+++ c/problems/876-middle-of-the-linked-list.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+struct ListNode {
+  int val;
+  ListNode* next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+class Solution {
+ public:
+  ListNode* middleNode(ListNode* head) {
+    ListNode* start = head;
+    ListNode* end = head;
+
+    while (end != NULL && end->next) {
+      end = end->next->next;
+      start = start->next;
+    }
+
+    return start;
+  }
+};
+
+int main() { return 0; }
