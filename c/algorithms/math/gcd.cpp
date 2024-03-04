8718d4d2afba135be738f7fd7290db40c44e9df2
Ta-Yi Cheng <admin@tranquility.tech>
2024-03-04 10:25:07 +0800

    update

---
+++ c/algorithms/math/gcd.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// https://hackmd.io/@jQSlhiN3QaGkugpbD4NDAA/rkvNkyQfi
+int gcd(int x, int y) { return y == 0 ? x : gcd(y, x % y); }
+
+int main() {
+  cout << gcd(20, 5);
+
+  return 0;
+}
