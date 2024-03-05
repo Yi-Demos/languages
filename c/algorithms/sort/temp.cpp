ddc46fb53c04da2df951488ff59d19bc34aca79a
Ta-Yi Cheng <admin@tranquility.tech>
2024-03-05 13:51:20 +0800

    update

--- c/algorithms/sort/temp.cpp
+++ c/algorithms/sort/temp.cpp
@@ -6,9 +6,9 @@ using namespace std;
 class Solution {
  public:
   vector<int> sort(vector<int> &nums) {
-    for (int i = 0; i < nums.size(); i++) {
-      for (int j = i + 1; j < nums.size(); j++) {
-        if (nums[i] > nums[i + 1]) swap(nums[i + 1], nums[i]);
+    for (int i = nums.size() - 1; i >= 0; --i) {
+      for (int j = 0; j < i; ++j) {
+        if (nums[j] > nums[j + 1]) swap(nums[j], nums[j + 1]);
       }
     }
 
