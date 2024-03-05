fab6619d42c0d2ab7f853649ca033451ac1ccab4
Ta-Yi Cheng <admin@tranquility.tech>
2024-03-06 04:13:25 +0800

    update

--- c/algorithms/sort/quick-sort.cpp
+++ c/algorithms/sort/quick-sort.cpp
@@ -4,22 +4,29 @@ void swap(int *a, int *b) {
   *a = *b;
   *b = temp;
 }
+
 int Partition(int *arr, int front, int end) {
   int pivot = arr[end];
   int i = front - 1;
+
   for (int j = front; j < end; j++) {
     if (arr[j] < pivot) {
       i++;
       swap(&arr[i], &arr[j]);
     }
   }
+
   i++;
+
   swap(&arr[i], &arr[end]);
+
   return i;
 }
+
 void QuickSort(int *arr, int front, int end) {
   if (front < end) {
     int pivot = Partition(arr, front, end);
+
     QuickSort(arr, front, pivot - 1);
     QuickSort(arr, pivot + 1, end);
   }
