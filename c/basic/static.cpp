13a2c6c5c9fb8fc09bafec5559cdb40962ea8951
Ta-Yi Cheng <admin@tranquility.tech>
2024-03-05 22:14:07 +0800

    update

--- c/basic/static.cpp
+++ c/basic/static.cpp
@@ -1,18 +1,19 @@
-#include <iostream>
+// https://ithelp.ithome.com.tw/articles/10308388
+#include <stdio.h>
 
-using namespace std;
+// function 內的 static 變數
+// 如果是宣告在 function 內的 static 變數，則該變數的生命週期不會因為離開
+// function 而清除該變數
 
-void hello();  // declaration
+void func(void) {
+  int a = 0;
+  static int b = 0;
 
-int main() {
-  for (int i = 0; i < 5; i++) hello();
-
-  return 0;
+  printf("a = %d, b = %d\n", ++a, ++b);
 }
 
-// definition
-void hello() {
-  static int x = 0;
-  printf("%d \n", x);
-  x++;
+int main(void) {
+  for (int i = 0; i < 10; i++) func();
+
+  return 0;
 }
