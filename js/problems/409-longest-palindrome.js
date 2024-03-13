33c182455836059bc93560328cec83cfdc77c857
Ta-Yi Cheng <admin@tranquility.tech>
2024-03-14 04:20:47 +0800

    update

---
+++ js/problems/409-longest-palindrome.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var longestPalindrome = function(s) {
+  const obj = {};
+  let sum = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    const key = s[i];
+
+    if (obj[key] == 1) {
+      sum+=2;
+      obj[key] = 0;
+    } else {
+      obj[key] = 1;
+    }
+  }
+
+  if (sum < s.length) sum++;
+
+  return sum;
+};
+
+console.log(longestPalindrome("aaab"));
