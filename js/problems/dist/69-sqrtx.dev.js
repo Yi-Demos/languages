f68b6ccf0341093ccbb0df31f8d9eb0bc5b9122a
Ta-Yi Cheng <admin@tranquility.tech>
2024-03-17 13:44:20 +0800

    update

---
+++ js/problems/dist/69-sqrtx.dev.js
@@ -0,0 +1,16 @@
+"use strict";
+
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function mySqrt(x) {// Method 1: start from min
+  // let i = 1;
+  // while (i * i <= x) {
+  //     i++;
+  // }
+  // return i - 1;
+  // Method 2: native fn
+  // return Math.floor(Math.sqrt(x));
+  // Method 3: binary search
+};
