54c4d7c8fc46873e0618ae09d298fa272f621e5b
Ta-Yi Cheng <admin@tranquility.tech>
2024-03-17 13:44:20 +0800

    update

---
+++ js/basic/round_num.js
@@ -0,0 +1,11 @@
+console.log(Math.floor(5.95));
+// Expected output: 5
+
+console.log(Math.floor(5.05));
+// Expected output: 5
+
+console.log(Math.floor(5));
+// Expected output: 5
+
+console.log(Math.floor(-5.05));
+// Expected output: -6
---
+++ js/problems/69-sqrtx.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function (x) {
+  // Method 1: start from min
+  // let i = 1;
+
+  // while (i * i <= x) {
+  //     i++;
+  // }
+
+  // return i - 1;
+
+  // Method 2: native fn
+  // return Math.floor(Math.sqrt(x));
+
+  // Method 3: binary search
+
+
+};
