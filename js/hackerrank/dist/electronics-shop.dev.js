4c85d2b479382fedb3ad26fc6e1906218399064a
Ta-Yi Cheng <admin@tranquility.tech>
2024-04-19 16:02:39 +0800

    update

---
+++ js/hackerrank/dist/electronics-shop.dev.js
@@ -0,0 +1,24 @@
+"use strict";
+
+function getMoneySpent(keyboards, drives, b) {
+  var result = -1;
+  keyboards.sort();
+  drives.sort();
+
+  for (var i = 0; i < keyboards.length; i++) {
+    for (var j = 0; j < drives.length; j++) {
+      var sum = keyboards[i] + drives[j];
+
+      if (b > sum && sum > result) {
+        result = sum;
+      }
+    }
+  }
+
+  console.log(result);
+  return result;
+}
+
+getMoneySpent([3, 1], [5, 2, 8], 10); // 9
+
+getMoneySpent([5], [4], 5); // -1
