002b4c9e833bbeac5417d2cff337d01578d676ff
Ta-Yi Cheng <admin@tranquility.tech>
2024-04-19 10:26:04 +0800

    update

---
+++ js/problems/dist/387-first-unique-character-in-a-string.dev.js
@@ -0,0 +1,11 @@
+"use strict";
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var firstUniqChar = function firstUniqChar(s) {};
+
+console.log(firstUniqChar("leetcode")); // 0
+
+console.log(firstUniqChar("loveleetcode")); // 2
