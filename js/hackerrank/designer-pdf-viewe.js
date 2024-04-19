aea42539b109fef08ef1ace9974f15f56f3ea31d
Ta-Yi Cheng <admin@tranquility.tech>
2024-04-19 10:26:04 +0800

    update

---
+++ js/hackerrank/designer-pdf-viewe.js
@@ -0,0 +1,21 @@
+function designerPdfViewer(h, word) {
+    let maxValue = 0;
+
+    for (const char of word) {
+      const index = char.charCodeAt(0) - 97; // convert letter to number a = 0 ...
+
+      if (h[index] > maxValue) maxValue = h[index];
+    }
+
+    return maxValue * word.length;
+}
+
+console.log(designerPdfViewer(
+  [1, 3, 1, 3, 1, 4, 1, 3, 2, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5],
+  "abc"
+)); // 9
+
+console.log(designerPdfViewer(
+  [1, 3, 1, 3, 1, 4, 1, 3, 2, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 7],
+  "zaba"
+));
