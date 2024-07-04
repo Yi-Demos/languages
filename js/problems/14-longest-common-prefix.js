71f0033270f7b1823f680d1cb449e900d813fba1
Ta-Yi Cheng <admin@tranquility.tech>
2024-07-04 09:58:51 +0800

    update

---
+++ js/problems/14-longest-common-prefix.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+  let ans = "";
+
+  for (let i = 0; i < strs.length; i++) {
+
+  }
+};
+
+console.log(longestCommonPrefix(["flower","flow","flight"])); // "fl"
+console.log(longestCommonPrefix(["dog","racecar","car"])); // ""
