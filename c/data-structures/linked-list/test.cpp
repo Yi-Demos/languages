6d739de60bfad52ccb5dbfe8215416bcc466ebdb
Ta-Yi Cheng <admin@tranquility.tech>
2024-11-29 11:09:48 +0800

    update

--- c/data-structures/linked-list/test.cpp
+++ c/data-structures/linked-list/test.cpp
@@ -2,22 +2,18 @@
 
 using namespace std;
 
-struct LL {
+struct Node {
   int value;
-  LL* next;
+  Node* next;
 };
 
-class TEST {
+class ClassTest {
   public:
-    TEST(): v1(1) {};
+    ClassTest(): v1(1) {};
 
     void p() {
       printf("%d \n", v1);
     };
-
-    void p(int v1) {
-      printf("%d \n", v1);
-    }
 
     void p(int v1) {
       printf("%d \n", v1);
@@ -28,18 +24,49 @@ class TEST {
     };
 
   private:
-    LL ll;
     int v1;
 };
 
-int main () {
-  TEST test;
-  int a = 99;
+class LinkTest {
+  public:
+    void print(Node node) {
+      Node *ptr=&node;
 
-  test.p();
-  test.r();
-  test.p();
-  test.p(a);
+      while (ptr != NULL)
+      {
+        printf("%d", ptr->value);
+        ptr=ptr->next;
+      }
+    }
+
+  private:
+};
+
+int main () {
+  // LinkTest
+  LinkTest Link;
+  Node node1, node2;
+
+  node1.value=1;
+  node2.value=2;
+
+  node1.next=&node2;
+  // if no declare node2.next=NULL, it shows "segmentation fault"
+  node2.next=NULL;
+
+  printf("%d\n", node1.value);
+  printf("%d\n", node2.value);
+
+  Link.print(node1);
+
+  // ClassTest
+  // ClassTest test;
+
+  // int a = 99;
+  // test.p();
+  // test.r();
+  // test.p();
+  // test.p(a);
 
   return 0;
 };
