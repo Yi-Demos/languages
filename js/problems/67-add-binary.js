c614d7563fab9c5ece2d64ff704993121b42ab74
Ta-Yi Cheng <admin@tranquility.tech>
2025-01-06 03:16:39 +0800

    update

---
+++ js/problems/67-add-binary.js
@@ -0,0 +1,33 @@
+/**
+ * @param {string} a
+ * @param {string} b
+ * @return {string}
+ */
+var addBinary = function(a, b) {
+  let carry = 0;
+  let strings = "";
+  let i = a.length-1;
+  let j = b.length-1;
+
+  while (i>=0 || j>=0 || carry) {
+    let sum = 0;
+
+    if (carry > 0) {
+      sum+=carry;
+      carry=0;
+    }
+
+    if (i>=0) sum+=Number(a[i]);
+    if (j>=0) sum+=Number(b[j]);
+
+    if (sum>1) carry=1;
+
+    sum=sum%2;
+    strings=sum.toString()+strings;
+
+    i--;
+    j--;
+  }
+
+  return strings;
+};
