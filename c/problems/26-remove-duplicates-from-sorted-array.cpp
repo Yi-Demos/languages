764abe7aa8a05e03a6171396baa2c8262b9bc01b
Ta-Yi Cheng <admin@tranquility.tech>
2025-01-30 04:01:39 +0800

    update

---
+++ c/problems/26-remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,12 @@
+int removeDuplicates(int* nums, int numsSize) {
+    int s = 0;
+
+    for (int f = 1; f < numsSize; f++) {
+        if (nums[s] != nums[f]) {
+            s++;
+            nums[s] = nums[f];
+        }
+    }
+
+    return s+1;
+}
