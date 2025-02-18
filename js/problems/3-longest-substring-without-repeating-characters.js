13aec4d7262f07aec206530292603706f8ea6c83
Ta-Yi Cheng <admin@tranquility.tech>
2025-02-19 04:51:21 +0800

    update

---
+++ js/problems/3-longest-substring-without-repeating-characters.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+  let set = new Set();
+  let l = 0;
+  let maxLength = 0;
+
+  for (let r = 0; r < s.length; r++) {
+      while (set.has(s[r])) {
+          set.delete(s[l]);
+          l++;
+      }
+
+      set.add(s[r]);
+      maxLength = Math.max(maxLength, r - l + 1);
+  }
+
+  return maxLength;
+};
+
+// lengthOfLongestSubstring("abcabcbb"); // 3
+// lengthOfLongestSubstring("bbbbb"); // 1
+lengthOfLongestSubstring("pwwkew"); // 3
+// lengthOfLongestSubstring(""); // 0
+// lengthOfLongestSubstring(" "); // 1
---
+++ js/problems/dist/3-longest-substring-without-repeating-characters.dev.js
@@ -0,0 +1,29 @@
+"use strict";
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function lengthOfLongestSubstring(s) {
+  var set = new Set();
+  var l = 0;
+  var maxLength = 0;
+
+  for (var r = 0; r < s.length; r++) {
+    while (set.has(s[r])) {
+      set["delete"](s[l]);
+      l++;
+    }
+
+    set.add(s[r]);
+    maxLength = Math.max(maxLength, r - l + 1);
+  }
+
+  return maxLength;
+}; // lengthOfLongestSubstring("abcabcbb"); // 3
+// lengthOfLongestSubstring("bbbbb"); // 1
+
+
+lengthOfLongestSubstring("pwwkew"); // 3
+// lengthOfLongestSubstring(""); // 0
+// lengthOfLongestSubstring(" "); // 1
