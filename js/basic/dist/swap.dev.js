986e30b0decdbb213bc9bee7d3444f46575965c1
Ta-Yi Cheng <admin@tranquility.tech>
2025-02-22 02:13:31 +0800

    update

--- js/basic/dist/swap.dev.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-var a = 1;
-var b = 2;
-var _ref = [b, a];
-a = _ref[0];
-b = _ref[1];
-console.log(a, b);
--- js/trick/dist/convert.dev.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-// number to binary
-(3).toString(2); // "11"
-// binary to number
-
-parseInt("11", 2); // 3
