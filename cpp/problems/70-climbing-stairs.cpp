9076e64e2f374118d94bd1ced8d3ee7b807b54b7
Ta-Yi Cheng <admin@tranquility.tech>
2025-02-24 22:45:47 +0800

    update

--- cpp/problems/70-climbing-stairs.cpp
+++ cpp/problems/70-climbing-stairs.cpp
@@ -21,6 +21,25 @@ class Solution {
   }
 };
 
+// dynamic programming
+class Solution {
+ public:
+  int climbStairs(int n) {
+    vector<int> dp(n + 1);
+
+    dp[0] = 1;
+    dp[1] = 1;
+
+    if (n <= 1) return dp[n];
+
+    for (int i = 2; i <= n; i++) {
+      dp[i] = dp[i - 2] + dp[i - 1];
+    }
+
+    return dp[n];
+  }
+};
+
 int main() {
   Solution solution;
 
