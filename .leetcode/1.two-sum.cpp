6b36d7dc2ce19c14abd84d67064e62c072e960ff
Ta-Yi Cheng <admin@tranquility.tech>
2025-02-26 22:39:44 +0800

    update

---
+++ .leetcode/1.two-sum.cpp
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=1 lang=cpp
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+class Solution {
+ public:
+  vector<int> twoSum(vector<int>& nums, int target) {
+    unordered_map<int, int> numsMap;
+
+    for (int i = 0; i < nums.size(); i++) {
+      int pairNum = target - nums[i];
+
+      if (numsMap.find(pairNum) == numsMap.end()) {
+        numsMap[nums[i]] = i;
+      } else {
+        return vector<int>{numsMap[pairNum], i};
+      }
+    }
+
+    return {};
+  }
+};
+// @lc code=end
