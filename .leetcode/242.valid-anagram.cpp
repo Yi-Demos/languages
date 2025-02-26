21eb7c80256c81ced6dfecbf3756b8562fbfadf7
Ta-Yi Cheng <admin@tranquility.tech>
2025-02-26 22:37:23 +0800

    update

---
+++ .leetcode/242.valid-anagram.cpp
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode id=242 lang=cpp
+ *
+ * [242] Valid Anagram
+ */
+
+// @lc code=start
+class Solution {
+ public:
+  bool isAnagram(string s, string t) {
+    // method 1: hashmap char counter
+    // time complexity: O(n)
+    // space complexity: O(n)
+    if (s.length() != t.length()) return false;
+
+    unordered_map<char, int> sCounter;
+
+    for (const char c : s) {
+      sCounter[c]++;
+    }
+
+    for (const char c : t) {
+      sCounter[c]--;
+    }
+
+    for (const char c : s) {
+      if (sCounter[c] != 0) return false;
+    }
+
+    return true;
+  }
+};
+// @lc code=end
