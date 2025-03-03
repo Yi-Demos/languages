c0aa12eb8463a5b093491a877b1f501afd579862
Ta-Yi Cheng <admin@tranquility.tech>
2025-03-03 15:51:54 +0800

    update

---
+++ .leetcode/121.best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,60 @@
+/*
+ * @lc app=leetcode id=121 lang=cpp
+ *
+ * [121] Best Time to Buy and Sell Stock
+ */
+
+// @lc code=start
+class Solution {
+ public:
+  int maxProfit(vector<int>& prices) {
+    // Approach 1: simple loop
+    // time complexity: O(n^2)
+    // space complexity: O(1)
+    // int max = 0;
+
+    // for (int i = 0; i < prices.size(); i++) {
+    //   for (int j = i + 1; j < prices.size(); j++) {
+    //     int sum = prices[j] - prices[i];
+
+    //     if (sum > max) {
+    //       cout << prices[i] << prices[j] << "\n";
+    //       max = sum;
+    //     }
+    //   }
+    // }
+
+    // return max;
+
+    // Approach 2: greedy
+    // time complexity: O(n)
+    // space complexity: O(1)
+    int maxProfit = 0;
+
+    for (int i = 1; i < prices.size(); i++) {
+      if (prices[i] > prices[i - 1]) maxProfit += prices[i] - prices[i - 1];
+    }
+
+    return maxProfit;
+
+    // Approach 3: simple loop
+    // time complexity: O(n)
+    // space complexity: O(1)
+
+    // int minPrice = INT_MAX;
+    // int maxProfit = 0;
+
+    // for (int price : prices) {
+    //   if (price < minPrice) {
+    //     minPrice = price;
+    //   } else if (price - minPrice > maxProfit) {
+    //     maxProfit = price - minPrice;
+    //   }
+
+    //   cout << price << ", " << minPrice << ", " << maxProfit << "\n";
+    // }
+
+    // return maxProfit;
+  }
+};
+// @lc code=end
