8028b6c75765f1cc4fd2ca8d5949c0ab549a2708
Ta-Yi Cheng <admin@tranquility.tech>
2025-03-04 11:33:23 +0800

    update

--- .leetcode/121.best-time-to-buy-and-sell-stock.cpp
+++ .leetcode/121.best-time-to-buy-and-sell-stock.cpp
@@ -29,32 +29,30 @@ class Solution {
     // Approach 2: greedy
     // time complexity: O(n)
     // space complexity: O(1)
-    int maxProfit = 0;
-
-    for (int i = 1; i < prices.size(); i++) {
-      if (prices[i] > prices[i - 1]) maxProfit += prices[i] - prices[i - 1];
-    }
-
-    return maxProfit;
-
-    // Approach 3: simple loop
-    // time complexity: O(n)
-    // space complexity: O(1)
-
-    // int minPrice = INT_MAX;
     // int maxProfit = 0;
 
-    // for (int price : prices) {
-    //   if (price < minPrice) {
-    //     minPrice = price;
-    //   } else if (price - minPrice > maxProfit) {
-    //     maxProfit = price - minPrice;
-    //   }
-
-    //   cout << price << ", " << minPrice << ", " << maxProfit << "\n";
+    // for (int i = 1; i < prices.size(); i++) {
+    //   if (prices[i] > prices[i - 1]) maxProfit += prices[i] - prices[i - 1];
     // }
 
     // return maxProfit;
+
+    // Approach 3:
+    // time complexity:
+    // space complexity:
+
+    int minPrice = INT_MAX;
+    int maxProfit = 0;
+
+    for (int price : prices) {
+      if (price < minPrice) {
+        minPrice = price;
+      } else if (price - minPrice > maxProfit) {
+        maxProfit = price - minPrice;
+      }
+    }
+
+    return maxProfit;
   }
 };
 // @lc code=end
---
+++ .leetcode/66.plus-one.cpp
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode id=66 lang=cpp
+ *
+ * [66] Plus One
+ */
+
+// @lc code=start
+class Solution {
+ public:
+  vector<int> plusOne(vector<int>& digits) {
+    // Approach 1: simple loop
+    // time complexity: O(n)
+    // space complexity: O(1)
+    digits[digits.size() - 1] += 1;
+
+    int carry = 0;
+
+    // adjust the carry
+    for (int i = digits.size() - 1; i >= 0; i--) {
+      int current = digits[i] + carry;
+
+      if (current >= 10) {
+        carry = 1;
+        digits[i] = current % 10;
+      } else {
+        carry = 0;
+        digits[i] = current;
+      }
+
+      if (carry > 0 && i == 0) {
+        digits.insert(digits.begin(), carry);
+      }
+    }
+
+    return digits;
+  }
+};
+// @lc code=end
