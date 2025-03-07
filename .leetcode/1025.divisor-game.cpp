6d02d51fbb43748340ff78ffca4bcfb4b94dca72
Ta-Yi Cheng <admin@tranquility.tech>
2025-03-08 01:09:58 +0800

    update

--- .leetcode/1025.divisor-game.cpp
+++ .leetcode/1025.divisor-game.cpp
@@ -7,6 +7,27 @@
 // @lc code=start
 class Solution {
  public:
-  bool divisorGame(int n) {}
+  bool divisorGame(int n) {
+    // Approach 1: Dynamic Programming
+    // time complexity: O(n^2)
+    // space complexity: O(n)
+    vector<bool> dp(n + 1, false);
+
+    for (int i = 2; i <= n; i++) {
+      for (int x = 1; x < i; x++) {
+        if (i % x == 0 && !dp[i - x]) {
+          dp[i] = true;
+          break;
+        }
+      }
+    }
+
+    return dp[n];
+
+    // Approach 2: Math
+    // time complexity: O(1)
+    // space complexity: O(1)
+    // return n % 2 == 0;
+  }
 };
 // @lc code=end
