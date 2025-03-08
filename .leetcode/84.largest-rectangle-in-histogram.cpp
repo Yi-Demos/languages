5c7d64bb33bacf01f36944309215e6642ae840c8
Ta-Yi Cheng <admin@tranquility.tech>
2025-03-09 00:36:07 +0800

    update

--- .leetcode/84.largest-rectangle-in-histogram.cpp
+++ .leetcode/84.largest-rectangle-in-histogram.cpp
@@ -24,21 +24,44 @@ class Solution {
 
   int largestRectangleArea(vector<int>& heights) {
     // Approach 1: iterative (Time Limit Exceeded)
+    // time complexity: O(n^2)
+    // space complexity: O(n)
+    // int largest = 0;
+
+    // for (int i = 0; i < heights.size(); i++) {
+    //   int height = heights[i];
+    //   int width = getWidth(heights, i, height);
+    //   int current = height * width;
+
+    //   cout << "height" << height << "width" << width << "\n";
+
+    //   largest = max(current, largest);
+    // }
+
+    // return largest;
+
+    // Approach 2: Monotonic Stack
     // time complexity: O(n)
-    // space complexity: O(1)
-    int largest = 0;
+    // space complexity: O(n)
+    // https://www.youtube.com/watch?v=XwUb7x6YDeA&ab_channel=LeetCode%E5%8A%9B%E6%89%A3
+    stack<int> indices;
+    int max_area = 0;
+    int n = heights.size();
 
-    for (int i = 0; i < heights.size(); i++) {
-      int height = heights[i];
-      int width = getWidth(heights, i, height);
-      int current = height * width;
+    for (int i = 0; i <= n; i++) {
+      int h = (i == n ? 0 : heights[i]);
 
-      cout << "height" << height << "width" << width << "\n";
+      while (!indices.empty() && h < heights[indices.top()]) {
+        int height = heights[indices.top()];
+        indices.pop();
+        int width = indices.empty() ? i : i - indices.top() - 1;
+        max_area = max(max_area, height * width);
+      }
 
-      largest = max(current, largest);
+      indices.push(i);
     }
 
-    return largest;
+    return max_area;
   }
 };
 // @lc code=end
