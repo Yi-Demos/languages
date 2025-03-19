a725cee9479bf2f731f1120625a9a1259b6e92e4
Ta-Yi Cheng <admin@tranquility.tech>
2025-03-19 12:40:15 +0800

    update

---
+++ .leetcode/62.unique-paths.cpp
@@ -0,0 +1,64 @@
+/*
+ * @lc app=leetcode id=62 lang=cpp
+ *
+ * [62] Unique Paths
+ */
+
+// @lc code=start
+class Solution {
+ public:
+  int uniquePaths(int m, int n) {
+    /*
+      Approach 1: Math
+      Time complexity: O(m+n)
+      Space complexity: O(1)
+    */
+
+    // int N = m + n - 2;
+    // int k = min(m - 1, n - 1);
+    // long long res = 1;
+
+    // for (int i = 1; i <= k; ++i) {
+    //   res = res * (N - k + i) / i;
+    // }
+
+    // return res;
+
+    /*
+      Approach 2: Dynamic Programming
+      Time complexity: O(m*n)
+      Space complexity: O(m*n)
+    */
+
+    vector<vector<int>> dp(m, vector<int>(n, 1));
+
+    for (int i = 1; i < m; ++i) {
+      for (int j = 1; j < n; ++j) {
+        dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+      }
+    }
+
+    return dp[m - 1][n - 1];
+
+    /*
+      Approach 3: Dynamic Programming && Recursive (Easy understanding)
+      Time complexity: O(m*n)
+      Space complexity: O(n)
+
+      https://www.youtube.com/watch?v=fmpP5Ll0Azc&ab_channel=HuaHua
+    */
+
+    // if (m == 0 || n == 0) return 0;
+    // if (m == 1 && n == 1) return 1;
+    // if (dp[m][n] > 0) return dp[m][n];
+
+    // int leftPath = uniquePaths(m - 1, n);
+    // int topPath = uniquePaths(m, n - 1);
+
+    // return leftPath + topPath;
+  }
+
+ private:
+  unordered_map<int, unordered_map<int, int>> dp;  // for Approach 3
+};
+// @lc code=end
---
+++ .leetcode/63.unique-paths-ii.cpp
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=63 lang=cpp
+ *
+ * [63] Unique Paths II
+ */
+
+// @lc code=start
+class Solution {
+ public:
+  int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+    /*
+
+    */
+  }
+};
+// @lc code=end
