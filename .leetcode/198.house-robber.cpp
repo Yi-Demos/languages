33d3fbc84820572b7ce041f0e9c4d3d220127868
Ta-Yi Cheng <admin@tranquility.tech>
2025-04-04 23:23:39 +0800

    update

--- .leetcode/198.house-robber.cpp
+++ .leetcode/198.house-robber.cpp
@@ -14,19 +14,39 @@ class Solution {
       Space Complexity: O(n)
     */
 
+    // if (nums.size() == 0) return 0;
+    // if (nums.size() == 1) return nums[0];
+
+    // vector<int> dp(nums.size(), 0);
+
+    // dp[0] = nums[0];
+    // dp[1] = max(nums[0], nums[1]);
+
+    // for (int i = 2; i < nums.size(); i++) {
+    //   dp[i] = max(dp[i - 1], dp[i - 2] + nums[i]);
+    // }
+
+    // return dp.back();
+
+    /*
+      Aproach: Dynamic Programming
+      Time Complexity: O(n)
+      Space Complexity: O(1)
+    */
+
     if (nums.size() == 0) return 0;
     if (nums.size() == 1) return nums[0];
+    if (nums.size() == 2) return min(nums[0], nums[1]);
 
-    vector<int> dp(nums.size(), 0);
-
-    dp[0] = nums[0];
-    dp[1] = max(nums[0], nums[1]);
+    int cur = 0;
+    int prev1 = nums[0];
+    int prev2 = nums[1];
 
     for (int i = 2; i < nums.size(); i++) {
-      dp[i] = max(dp[i - 1], dp[i - 2] + nums[i]);
+      cur = min(prev1, nums[i] + prev2);
     }
 
-    return dp.back();
+    return cur;
   }
 };
 // @lc code=end
--- .leetcode/213.house-robber-ii.cpp
+++ .leetcode/213.house-robber-ii.cpp
@@ -7,6 +7,42 @@
 // @lc code=start
 class Solution {
  public:
-  int rob(vector<int>& nums) {}
+  int cal(vector<int> nums) {
+    // int prev2 = 0, prev1 = 0;
+
+    // for (int num : nums) {
+    //   int curr = max(prev1, prev2 + num);
+
+    //   prev2 = prev1;
+    //   prev1 = curr;
+    // }
+
+    // return prev1;
+
+    if (nums.size() == 0) return 0;
+    if (nums.size() == 1) return nums[0];
+
+    vector<int> dp(nums.size(), 0);
+
+    dp[0] = nums[0];
+    dp[1] = max(nums[0], nums[1]);
+
+    for (int i = 2; i < nums.size(); i++) {
+      dp[i] = max(dp[i - 1], dp[i - 2] + nums[i]);
+    }
+
+    return dp.back();
+  }
+  int rob(vector<int>& nums) {
+    int n = nums.size();
+
+    if (n == 0) return 0;
+    if (n == 1) return nums[0];
+
+    vector<int> case1(nums.begin(), nums.end() - 1);
+    vector<int> case2(nums.begin() + 1, nums.end());
+
+    return max(cal(case1), cal(case2));
+  }
 };
 // @lc code=end
