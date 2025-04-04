231e3088b9b11f0aa1eb544a7442882d297b4593
Ta-Yi Cheng <admin@tranquility.tech>
2025-04-05 04:38:08 +0800

    update

--- .leetcode/241.different-ways-to-add-parentheses.cpp
+++ .leetcode/241.different-ways-to-add-parentheses.cpp
@@ -7,6 +7,39 @@
 // @lc code=start
 class Solution {
  public:
-  vector<int> diffWaysToCompute(string expression) {}
+  unordered_map<string, vector<int>> memo;
+
+  vector<int> diffWaysToCompute(string expression) {
+    if (memo.count(expression)) return memo[expression];
+
+    vector<int> res;
+
+    for (int i = 0; i < expression.size(); i++) {
+      if (expression[i] == '+' || expression[i] == '-' ||
+          expression[i] == '*') {
+        vector<int> left = diffWaysToCompute(expression.substr(0, i));
+        vector<int> right =
+            diffWaysToCompute(expression.substr(i + 1, expression.size()));
+
+        for (int l : left) {
+          for (int r : right) {
+            if (expression[i] == '+') {
+              res.push_back(l + r);
+            } else if (expression[i] == '-') {
+              res.push_back(l - r);
+            } else if (expression[i] == '*') {
+              res.push_back(l * r);
+            }
+          }
+        }
+      }
+    }
+
+    if (res.empty()) res.push_back(stoi(expression));
+
+    memo[expression] = res;
+
+    return res;
+  }
 };
 // @lc code=end
