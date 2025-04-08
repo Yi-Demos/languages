ae201fcbd6d7a5960f7c2eb0f5c58ed83b1d2acf
Ta-Yi Cheng <admin@tranquility.tech>
2025-04-09 02:29:31 +0800

    update

---
+++ .leetcode/495.teemo-attacking.cpp
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode id=495 lang=cpp
+ *
+ * [495] Teemo Attacking
+ */
+
+// @lc code=start
+class Solution {
+ public:
+  int findPoisonedDuration(vector<int>& timeSeries, int duration) {
+    /*
+      Approach 1: Iterative
+      time complexity: O(n)
+      space complexity: O(1)
+    */
+
+    if (timeSeries.size() == 1) return duration;
+
+    int sum = 0;
+
+    for (int i = 1; i < timeSeries.size(); i++) {
+      sum += min(timeSeries[i] - timeSeries[i-1], duration);
+    }
+
+    sum+= duration;
+
+    return sum;
+  }
+};
+// @lc code=end
---
+++ .leetcode/605.can-place-flowers.cpp
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode id=605 lang=cpp
+ *
+ * [605] Can Place Flowers
+ */
+
+// @lc code=start
+class Solution {
+ public:
+  bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+    /*
+      Approach 1: Iterative
+      time complexity: O(n)
+      space complexity: O(1)
+    */
+
+    int count = 0;
+
+    for (int i = 0; i < flowerbed.size(); i++) {
+      bool isEmptyPos = flowerbed[i] == 0;
+      bool isLeftEmpty = (i == 0) || flowerbed[i - 1] == 0;
+      bool isRightEmpty = (i == flowerbed.size() - 1) || flowerbed[i + 1] == 0;
+
+      if (isEmptyPos && isLeftEmpty && isRightEmpty) {
+        count++;
+
+        flowerbed[i] = 1; // 種植，因此下一個就不能連續
+      }
+    }
+
+    return count >= n;
+  }
+};
+// @lc code=end
