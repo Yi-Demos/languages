0098b7963de7860aa52a4cc932daf55fef5c4170
Ta-Yi Cheng <admin@tranquility.tech>
2025-04-24 14:08:01 +0800

    update

--- .leetcode/7.reverse-integer.cpp
+++ .leetcode/7.reverse-integer.cpp
@@ -8,17 +8,36 @@
 class Solution {
  public:
   int reverse(int x) {
-    if (x < 0) x * -1;
+    /*
+      Approach 1: Hash
+      time complexity: O(n)
+      space complexity: O(1)
+    */
 
-    int digit = 0;
+    bool isNegative = false;
 
-    while ((x / 10 ^ digit) < 10) {
-      digit++;
+    if (x < 0) {
+      isNegative = true;
+      x * -1;
     }
 
-    cout << digit << endl;
+    string xString = to_string(x);
 
-    return 0;
+    int startIndex = 0;
+
+    while (xString[startIndex] == '0') {
+      startIndex++;
+    }
+
+    string xStringReversed = "";
+
+    for (int i = startIndex; i < xString.size() - startIndex; i++) {
+      xStringReversed.insert(0, 1, xString[i]);
+    }
+
+    int res = isNegative ? stoi(xStringReversed) * -1 : stoi(xStringReversed);
+
+    return res;
   }
 };
 // @lc code=end
