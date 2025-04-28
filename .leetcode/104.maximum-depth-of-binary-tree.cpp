f7fb55bef2fd4757553d3fbd5390ee2ab633d262
Ta-Yi Cheng <admin@tranquility.tech>
2025-04-28 14:01:46 +0800

    update

--- .leetcode/104.maximum-depth-of-binary-tree.cpp
+++ .leetcode/104.maximum-depth-of-binary-tree.cpp
@@ -20,12 +20,46 @@
 class Solution {
  public:
   int maxDepth(TreeNode* root) {
+    /*
+      Approach 1: DFS
+      time complexity: O(n)
+      space complexity: O(n)
+    */
+
+    // if (root == nullptr) return 0;
+
+    // int l = maxDepth(root->left);
+    // int r = maxDepth(root->right);
+
+    // return max(l, r) + 1;
+
+    /*
+      Approach 2: BFS
+      time complexity: O(n)
+      space complexity: O(n)
+    */
+
     if (root == nullptr) return 0;
 
-    int l = maxDepth(root->left);
-    int r = maxDepth(root->right);
+    queue<TreeNode*> q;
+    q.push(root);
+    int depth = 0;
 
-    return max(l, r) + 1;
+    while (!q.empty()) {
+      int levelWidth = q.size();
+
+      for (int i = 0; i < levelWidth; i++) {
+        TreeNode* node = q.front();
+        q.pop();
+
+        if (node->left) q.push(node->left);
+        if (node->right) q.push(node->right);
+      }
+
+      depth++;
+    }
+
+    return depth;
   }
 };
 // @lc code=end
--- .leetcode/110.balanced-binary-tree.cpp
+++ .leetcode/110.balanced-binary-tree.cpp
@@ -20,10 +20,48 @@
 class Solution {
  public:
   bool isBalanced(TreeNode* root) {
-    if (root == nullptr) return false;
-    if (root->left || root->right) isBalanced(root->left) && isBalanced(root->right);
+    /*
+      Approach 1: DFS
+      time complexity: O(n²)
+      space complexity: O(1)
+    */
 
-    return true;
+    // if (!root) return true;
+
+    // int leftHeight = maxDepth(root->left);
+    // int rightHeight = maxDepth(root->right);
+
+    // if (abs(leftHeight - rightHeight) > 1) return false;
+
+    // return isBalanced(root->left) && isBalanced(root->right);
+
+    /*
+      Approach 2: DFS
+      time complexity: O(n)
+      space complexity: O(n)
+    */
+
+    return checkHeight(root) != -1;
+  }
+
+ private:
+  int maxDepth(TreeNode* node) {
+    if (!node) return 0;
+
+    return 1 + max(maxDepth(node->left), maxDepth(node->right));
+  }
+  int checkHeight(TreeNode* node) {
+    if (!node) return 0;
+
+    int leftHeight = checkHeight(node->left);
+    if (leftHeight == -1) return -1;  // 左子樹不平衡
+
+    int rightHeight = checkHeight(node->right);
+    if (rightHeight == -1) return -1;  // 右子樹不平衡
+
+    if (abs(leftHeight - rightHeight) > 1) return -1;  // 當前節點不平衡
+
+    return max(leftHeight, rightHeight) + 1;  // 返回當前節點的高度
   }
 };
 // @lc code=end
