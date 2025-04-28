d554a2b0a85f05feb3f33a89d0cc405540f62520
Ta-Yi Cheng <admin@tranquility.tech>
2025-04-28 21:44:14 +0800

    update

--- .leetcode/2.add-two-numbers.cpp
+++ .leetcode/2.add-two-numbers.cpp
@@ -17,6 +17,36 @@
  */
 class Solution {
  public:
-  ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {}
+  ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+    /*
+      Approach 1: None
+      time complexity: O(n)
+      space complexity: O(1)
+    */
+
+    ListNode* dummy = new ListNode(0);
+    ListNode* current = dummy;
+    int carry = 0;
+
+    while (l1 != nullptr || l2 != nullptr || carry != 0) {
+      int sum = carry;
+
+      if (l1 != nullptr) {
+        sum += l1->val;
+        l1 = l1->next;
+      }
+
+      if (l2 != nullptr) {
+        sum += l2->val;
+        l2 = l2->next;
+      }
+
+      carry = sum / 10;
+      current->next = new ListNode(sum % 10);
+      current = current->next;
+    }
+
+    return dummy->next;
+  }
 };
 // @lc code=end
